70a6fce032bccfb73bc5fbac1c30e6f86749d690
Aewit <134361499+Aewit@users.noreply.github.com>
2024-05-23 12:04:16 +0800

    steveeenn

--- view/index.php
+++ view/index.php
@@ -1,8 +1,5 @@
 <?php include '../includes/header.php' ?>
 
 <!-- codes -->
-<<<<<<< HEAD
 stevenssss
-=======
->>>>>>> d29f70898f868a1d557f521638127aa718c56137
 <?php include '../includes/footer.php' ?>
