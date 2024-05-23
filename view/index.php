6b196ee20c0510a0c575b0ea5a049015daf94d59
Crescian <lcoryed@gmail.com>
2024-05-23 11:35:07 +0800

    test

--- view/index.php
+++ view/index.php
@@ -1,9 +1,5 @@
 <?php include '../includes/header.php' ?>
 
 <!-- codes -->
-<<<<<<< HEAD
-lloyd
-=======
-Crescian
->>>>>>> 667cb3ad75850e6be68f54d4e36eb04dd75d8f3f
+Main
 <?php include '../includes/footer.php' ?>
